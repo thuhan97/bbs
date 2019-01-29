bd032b05a0c3623d3faf20fd5c283c4f9f29522c
Do Nam [JVB] <do.hoangnam9x.jvb@gmail.com>
2019-01-29 17:03:55 +0700

    Fixing query for approve absence list

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Services\Contracts\IDayOffService;
 use App\Services\Contracts\IUserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class UserController extends Controller
 {
@@ -45,10 +46,8 @@ class UserController extends Controller
 		$listDate = $this->userDayOff->findList($request, $conditions);
 
 		$paginateData = $listDate->toArray();
-		$recordPerPage = $request->only('per_page');
-		$recordPerPage = $recordPerPage['per_page'] ?? null;
-		$queryApprove = $request->only('approve');
-		$queryApprove = $queryApprove['approve'] ?? null;
+		$recordPerPage = $request->get('per_page');
+		$queryApprove = $request->get('approve');
 		$approve = $queryApprove !== null ? ($queryApprove != 1 && $queryApprove != 0 ? null : $queryApprove) : null;
 
 		$availableDayLeft = $this->userDayOff->getDayOffUser(Auth::id());
@@ -59,16 +58,18 @@ class UserController extends Controller
 	{
 		$approvalUser = new User();
 		$approvalUser->approverUsers()->where('id', Auth::id())->first();
-		$isApproval = false;
+		$isApproval = true;
 		if ($approvalUser !== null && $approvalUser->id !== null) {
 			$isApproval = true;
 		}
 
 		// If user is approved
-		$conditions = ['day_offs.status'=>0];
+		$conditions = ['start_at'=>0];
+		$request->request->add(['year'=>Date('Y')]);
 		$search = $criterias['search'] ?? '';
-		$records = $this->userDayOff->findList($request, $conditions, ['*'], $search, $perPage)->toArray();
-		return view('end_user.user.day_off_approval',compact('isApproval', 'records'));
+		$totalRecord = $this->userDayOff->findList($request, [], ['*'], $search, $perPage)->toArray();
+//		$totalRecord = $this->userDayOff->findList($request, $conditions, ['*'], $search, $perPage)->toArray();
+		return view('end_user.user.day_off_approval',compact('isApproval', 'totalRecord'));
 	}
 
 	public function contact(Request $request)

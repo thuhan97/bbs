61ceec3f89f3abc8c9ccdbdee44ca32a694ab6ae
Thai <trongthai@jvb-corp.com>
2019-01-30 14:43:11 +0700

    [end_user]work_time

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Services\Contracts\IUserService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\WorkTime;
 
 class UserController extends Controller
 {
@@ -29,9 +31,50 @@ class UserController extends Controller
         return view('end_user.user.change_password');
     }
 
-    public function workTime()
+    public function workTime(Request $request)
     {
-        return view('end_user.user.work_time');
+        $late = $early = $ot = 0;
+//        echo "<pre>"; print_r(Auth::user()->id);die;
+        $month = $request->input('month');
+        $type = $request->input('type');
+        $t = array();
+        if($type == 'di_muon'){
+            $t = array(1, 3, 5);
+        }elseif ($type == 've_som'){
+            $t = array(2, 3);
+        }elseif ($type == 'ot'){
+            $t = array(4, 5);
+        }
+        $month = isset($month) ? $month : date('m');
+//        $list_work_times = WorkTime::where('user_id', Auth::user()->id)->whereMonth('work_day', $month);
+        if(!empty($t)){
+            $list_work_times = WorkTime::where('user_id', Auth::user()->id)->whereIn('type', $t)->whereMonth('work_day', $month)->get();
+//            print_r($type);die;
+            if($type == 'di_muon'){
+                $late = count($list_work_times);
+            }elseif ($type == 've_som'){
+                $early = count($list_work_times);
+            }elseif ($type == 'ot'){
+                $ot = count($list_work_times);
+            }
+        }else{
+            $list_work_times = WorkTime::where('user_id', Auth::user()->id)->whereMonth('work_day', $month)->get();
+            foreach ($list_work_times as $work_time){
+                if($work_time['type'] == 1 || $work_time['type'] == 5 || $work_time['type'] == 3){
+                    $late++;
+                }
+                if($work_time['type'] == 2 || $work_time['type'] == 3){
+                    $early++;
+                }
+                if($work_time['type'] == 4 || $work_time['type'] == 5){
+                    $ot++;
+                }
+            }
+        }
+//        echo "<pre>";print_r($list_work_times);die;
+
+//        echo "<pre>"; print_r($list_work_times);die;
+        return view('end_user.user.work_time', compact('list_work_times', 'late', 'early', 'ot'));
     }
 
     public function dayOff()
--- resources/views/end_user/user/work_time.blade.php
+++ resources/views/end_user/user/work_time.blade.php
@@ -1,7 +1,129 @@
+@php
+$url = $_SERVER['REQUEST_URI'];
+preg_match('/month=([0-9]+)/', $url, $m);
+$m = isset($m[1]) ? $m[1] : 0;
+@endphp
 @extends('layouts.end_user')
 @section('breadcrumbs')
     {!! Breadcrumbs::render('work_time') !!}
 @endsection
 @section('content')
-    Hôm nay bạn đến lúc 7h50
+    <div class="select-month">
+        <select class="w-22 mr-1 browser-default custom-select" name="month" onchange="document.location.href='/thoi-gian-lam-viec?month='+this.value">
+            <option value="">Chọn tháng</option>
+            @for($i=1; $i<=12; $i++)
+                <option value="{{$i}}" @if($i==$m) selected @endif>Tháng {{$i}}</option>
+            @endfor
+        </select>
+    </div>
+    <div class="list-work-time">
+        <div class="row statistic">
+            <form method="get">
+                <input hidden name="month">
+                <input hidden name="type" value="di_muon">
+                <button type="button" class="btn btn-danger" onclick="exec_submit(0)">Số buổi đi muộn: {{$late}}</button>
+            </form>
+            <form method="get">
+                <input hidden name="month">
+                <input hidden name="type" value="ve_som">
+                <button type="button" class="btn btn-primary" onclick="exec_submit(1)">Số buổi về sớm: {{$early}}</button>
+            </form>
+            <form method="get">
+                <input hidden name="month">
+                <input hidden name="type" value="ot">
+                <button type="button" class="btn btn-success" onclick="exec_submit(2)">Số buổi OT: {{$ot}}</button>
+            </form>
+        </div>
+        <table class="table table-bordered">
+            <thead>
+            <tr>
+                <th>
+                    Ngày làm việc
+                </th>
+                <th>
+                    Giờ đến công ty
+                </th>
+                <th>
+                    Giờ rời công ty
+                </th>
+                <th>
+                    Note
+                </th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($list_work_times as $work_time)
+                <tr>
+                    <td>
+                        {{$work_time['work_day']}}
+                    </td>
+                    <td>
+                        {{$work_time['start_at']}}
+                    </td>
+                    <td>
+                        {{$work_time['end_at']}}
+                    </td>
+                    <td>
+                        {{$work_time['note']}}
+                    </td>
+                    <th>
+                        <a class="feedback" data-toggle="modal" data-target="#feedback" data-id="{{$work_time['id']}}">Kêu oan</a>
+                    </th>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="feedback" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">ĐƠN KÊU OAN</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <textarea cols="55" rows="10"></textarea>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary complain">Đâm đơn</button>
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Bỏ qua</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        var id = 0;
+        function exec_submit(i) {
+            var f = document.forms[i];
+            f.month.value = getParam('month');
+            f.action = "/thoi-gian-lam-viec";
+            f.submit();
+        }
+
+        function getParam(param) {
+            var results = new RegExp('[\?&]' + param + '=([^&#]*)').exec(window.location.href);
+            if (results==null) {
+                return null;
+            }
+            return decodeURI(results[1]) || 0;
+        }
+
+        $('.feedback').click(function () {
+           id = $(this).attr('data-id')
+        });
+
+        $('.complain').click(function () {
+            var message = $('.modal-body textarea').val();
+            console.log(id);
+            $('#feedback').modal('hide');
+        });
+    </script>
+
 @endsection

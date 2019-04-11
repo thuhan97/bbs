7766f71508d4694fb097ab60e5328dbc9ba874ca
Tung Thanh <tung.jvb@gmail.com>
2019-04-11 14:39:58 +0700

    fix code

--- resources/views/end_user/regulation/index.blade.php
+++ resources/views/end_user/regulation/index.blade.php
@@ -19,8 +19,7 @@
         <ol class="list-group list-group-flush">
             @foreach($regulations as $idx => $regulation)
                 <li class="list-group-item no-border p-0 list-reulation">
-                    {{$idx + 1}}. <a class="text-dark"
-                            href="{{route('regulation_detail', ['id' => $regulation->id])}}">{{$regulation->name}}</a>
+                    {{$idx + 1}}. <a class="text-dark" href="{{route('regulation_detail', ['id' => $regulation->id])}}">{{$regulation->name}}</a>
                 </li>
             @endforeach
         </ol>

2a47237399e0df57bb084d17dc226bd334837327
Tung Thanh <tung.jvb@gmail.com>
2019-04-12 10:32:24 +0700

    fix code]

--- database/migrations/2019_04_11_102343_change_end_remove_column_to_remain_dayoffs.php
+++ database/migrations/2019_04_11_102343_change_end_remove_column_to_remain_dayoffs.php
@@ -14,9 +14,8 @@ class ChangeEndRemoveColumnToRemainDayoffs extends Migration
     public function up()
     {
         Schema::table('remain_dayoffs', function (Blueprint $table) {
-            $table->renameColumn('current_year', 'year');
-            $table->dropColumn('previous_year');
-            $table->integer('current_year')->default(0)->change();
+            $table->integer('previous_year')->default(0)->change();
+            $table->integer('current_year')->default(1)->change();
         });
     }
 

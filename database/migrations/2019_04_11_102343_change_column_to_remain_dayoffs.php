e1ece284f3987795aa62fac3847e0d270dee7eba
Tung Thanh <tung.jvb@gmail.com>
2019-04-12 16:34:43 +0700

    change migrate

--- database/migrations/2019_04_11_102343_change_column_to_remain_dayoffs.php
+++ database/migrations/2019_04_11_102343_change_column_to_remain_dayoffs.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ChangeEndRemoveColumnToRemainDayoffs extends Migration
+class ChangeColumnToRemainDayoffs extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,10 @@ class ChangeEndRemoveColumnToRemainDayoffs extends Migration
     public function up()
     {
         Schema::table('remain_dayoffs', function (Blueprint $table) {
+            $table->renameColumn('current_year', 'year');
+            $table->renameColumn('previous_year', 'remain');
             $table->integer('previous_year')->default(0)->change();
-            $table->integer('current_year')->default(1)->change();
+            $table->integer('current_year')->default(0)->change();
         });
     }
 

492aee815ae5116c415425e26bf4426b12aa7450
Tung Thanh <tung.jvb@gmail.com>
2019-04-22 18:50:29 +0700

    fix day off admin

--- resources/views/admin/day_off/form.blade.php
+++ resources/views/admin/day_off/form.blade.php
@@ -17,12 +17,14 @@
             <input type="hidden" name="user_id" value="{{ $record->user_id  }}">
         @endif
         <div class="col-md-12">
-            <div class="form-group margin-b-5 margin-t-5{{ $errors->has('title') ? ' has-error' : '' }}">
-                <label for="title">Tiêu đề</label>
-                {{ Form::select('title', VACATION, $record->title, ['class' => 'form-control my-1 mr-1 browser-default custom-select md-form select-item']) }}
-                @if ($errors->has('title'))
+            <div class="form-group margin-b-5 margin-t-5{{ $errors->has('number_off') ? ' has-error' : '' }}">
+                <label for="number_off">Số ngày nghỉ được tính (1 ngày hoặc nửa ngày) *</label>
+                <input type="text" class="form-control" name="number_off" placeholder="Số ngày phép bị trừ"
+                       value="{{ old('number_off', $record->number_off) }}">
+
+                @if ($errors->has('number_off'))
                     <span class="help-block">
-                    <strong>{{ $errors->first('title') }}</strong>
+                    <strong>{{ $errors->first('number_off') }}</strong>
                 </span>
                 @endif
             </div>
@@ -70,8 +72,19 @@
             </div>
 
         </div>
-
         <!-- /.col-md-12 -->
+        <div class="col-md-12">
+            <div class="form-group margin-b-5 margin-t-5{{ $errors->has('title') ? ' has-error' : '' }}">
+                <label for="title">Tiêu đề</label>
+                {{ Form::select('title', VACATION, $record->title, ['class' => 'form-control my-1 mr-1 browser-default custom-select md-form select-item']) }}
+                @if ($errors->has('title'))
+                    <span class="help-block">
+                    <strong>{{ $errors->first('title') }}</strong>
+                </span>
+                @endif
+            </div>
+            <!-- /.form-group -->
+        </div>
         <div class="col-md-12">
             <div class="form-group margin-b-5 margin-t-5{{ $errors->has('reason') ? ' has-error' : '' }}">
                 <label for="reason">Nhập lý do nghỉ phép *</label>
@@ -106,17 +119,6 @@
             <!-- /.form-group -->
         </div>
         <div class="col-md-12">
-            <div class="form-group margin-b-5 margin-t-5{{ $errors->has('number_off') ? ' has-error' : '' }}">
-                <label for="number_off">Số ngày nghỉ được tính (1 ngày hoặc nửa ngày) *</label>
-                <input type="text" class="form-control" name="number_off" placeholder="Số ngày phép bị trừ"
-                       value="{{ old('number_off', $record->number_off) }}">
-
-                @if ($errors->has('number_off'))
-                    <span class="help-block">
-                    <strong>{{ $errors->first('number_off') }}</strong>
-                </span>
-                @endif
-            </div>
             <!-- /.form-group -->
         </div>
         <div class="col-md-12">

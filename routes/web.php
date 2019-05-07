d0633389c2b8bb4df20dc11a56f9b47916d8a410
Tung Thanh <tung.jvb@gmail.com>
2019-05-07 18:31:06 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::group([
     Route::get('/tim-kiem-ngay-nghi/', 'UserController@dayOffSearch')->name('day_off_search');
     Route::get('/chi-tiet-ngay-nghi/{id?}/{check?}', 'UserController@dayOffDetail')->name('day_off_detail');
     Route::post('/chinh-sua-ngay-nghi/{id?}', 'UserController@editDayOffDetail')->name('edit_day_off_detail');
-    Route::post('/xoa-don-xin-nghi/', 'UserController@deleteDayOff')->name('delete_day_off');
+    Route::post('/xoa-don-xin-nghi/', 'UserController@deleteOrCloseDayOff')->name('delete_day_off');
 
     Route::get('/cong-no', 'PunishesController@index')->name('punish');
     Route::get('/noi-quy-quy-dinh', 'RegulationController@index')->name('regulation');

906154d8dbf73b09429d7a21b2b0d3bc27138168
Trình <trinhnv.jvb@gmail.com>
2019-05-21 10:20:10 +0700

    responsive

--- resources/views/end_user/event/detail.blade.php
+++ resources/views/end_user/event/detail.blade.php
@@ -31,84 +31,77 @@
             <!-- Text -->
             <p class="card-text py-2">{!! $event->content !!}</p>
             <!-- Button -->
-            <div class="text-center">
-                <p>
-                    <b>{{$event->place}}</b>, {{ $event->event_date }}
-                </p>
-            </div>
-            <hr/>
-
-            @if($event->deadline_at > date('Y-m-d H:i:s'))
-                <form class="border border-light p-5" method="POST" action="{{ route('join_event') }}">
-                    <p class="h4 mb-4 text-center">Phản hồi</p>
-                    <div class="d-flex justify-content-between joinEvent">
-                        @csrf
-                        <input type="hidden" name="event_id" value="{{ $event->id }}">
-                        <input type="hidden" name="deadline_at" value="{{ $event->deadline_at }}">
-                        <!-- Group of material radios - option 1 -->
-                        <div class="form-check">
-                            <input type="radio" class="form-check-input" id="materialGroupExample1"
-                                   name="status" value="1" checked>
-                            <label class="form-check-label" for="materialGroupExample1">Tham gia</label>
-                        </div>
-
-                        <!-- Group of material radios - option 2 -->
-                        <div class="form-check">
-                            <input type="radio" class="form-check-input" id="materialGroupExample2"
-                                   name="status" value="0">
-                            <label class="form-check-label" for="materialGroupExample2">Không tham </label>
-                        </div>
-                    </div>
-                    <br>
-                    <label for="exampleFormControlTextarea3">Ý kiến cá nhân:</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea3" rows="7"
-                              name="content"></textarea>
-                    <button class="btn btn-info btn-block my-4" type="submit">Gửi phản hồi</button>
-                </form>
-            @else
-                <h3 class="card-title h3 my-4 text-center text-danger"><strong>Đã hết thời hạn gửi phản hồi</strong>
-                </h3>
-            @endif
-            @if (count($listUserJoinEvent) > 0)
-                <h5 id="registerList" class="card-title h6 my-4"><b>Danh sách nhân viên đã đăng ký:</b></h5>
-                <table id="dtBasicExample" class="table table-striped table-bordered" cellspacing="0" width="100%">
-                    <thead>
-                    <tr>
-                        <th class="th-sm">Tên nhân viên
-                        </th>
-                        <th class="th-sm">Mã nhân viên
-                        </th>
-                        <th class="th-sm">Trạng thái
-                        </th>
-                        <th class="th-sm">Ý kiến cá nhân
-                        </th>
-                        <th class="th-sm">Ngày đăng kí
-                        </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($listUserJoinEvent as $listUserJoinEventValue)
-                        <tr>
-                            <td>{{ $listUserJoinEventValue->name }}</td>
-                            <td>{{ $listUserJoinEventValue->staff_code }}</td>
-                            <td>{{ $listUserJoinEventValue->status == 1 ? STATUS_JOIN_EVENT[1] : STATUS_JOIN_EVENT[0] }}</td>
-                            <td>{{ $listUserJoinEventValue->content}}</td>
-                            <td>{{ $listUserJoinEventValue->created_at }}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            @endif
-
         </div>
     </div>
-    @push('extend-js')
-        <script>
-            $(document).ready(function () {
-                $('#dtBasicExample').DataTable();
-                $('.dataTables_length').addClass('bs-select');
-            });
-        </script>
-    @endpush
+    @if($event->deadline_at > date('Y-m-d H:i:s'))
+        <form class="border border-light p-2 p-sm-5" method="POST" action="{{ route('join_event') }}">
+            <p class="h4 mb-4 text-center">Phản hồi</p>
+            <div class="d-flex justify-content-between joinEvent">
+                @csrf
+                <input type="hidden" name="event_id" value="{{ $event->id }}">
+                <input type="hidden" name="deadline_at" value="{{ $event->deadline_at }}">
+                <!-- Group of material radios - option 1 -->
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" id="materialGroupExample1"
+                           name="status" value="1" checked>
+                    <label class="form-check-label" for="materialGroupExample1">Tham gia</label>
+                </div>
+
+                <!-- Group of material radios - option 2 -->
+                <div class="form-check">
+                    <input type="radio" class="form-check-input" id="materialGroupExample2"
+                           name="status" value="0">
+                    <label class="form-check-label" for="materialGroupExample2">Không tham gia</label>
+                </div>
+            </div>
+            <br>
+            <label for="exampleFormControlTextarea3">Ý kiến cá nhân:</label>
+            <textarea class="form-control" id="exampleFormControlTextarea3" rows="7"
+                      name="content"></textarea>
+            <button class="btn btn-info btn-block my-4" type="submit">Gửi phản hồi</button>
+        </form>
+    @else
+        <h3 class="card-title h3 my-4 text-center text-danger"><strong>Đã hết thời hạn đăng ký - {{$event->deadline_at}}</strong>
+        </h3>
+    @endif
+    @if ($listUserJoinEvent->count() > 0)
+        <h5 id="registerList" class="card-title h6 my-4"><b>Đã
+                có {{$listUserJoinEvent->where('status', EVENT_JOIN_STATUS)->count()}}/{{$listUserJoinEvent->count()}}
+                nhân viên tham gia sự kiện:</b></h5>
+        <table id="dtBasicExample" class="table table-striped table-bordered" cellspacing="0" width="100%">
+            <thead>
+            <tr>
+                <th class="d-none d-sm-table-cell" style="width: 250px">Ngày đăng kí
+                </th>
+                <th class="th-sm" style="width: 250px">Tên nhân viên
+                </th>
+                <th class="th-sm" style="width: 150px">Trạng thái
+                </th>
+                <th class="d-none d-sm-table-cell">Ý kiến cá nhân
+                </th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($listUserJoinEvent as $listUserJoinEventValue)
+                <tr>
+                    <td class="d-none d-sm-table-cell">{{ $listUserJoinEventValue->created_at }}</td>
+                    <td>{{ $listUserJoinEventValue->name }}</td>
+                    <td>{{ $listUserJoinEventValue->status == 1 ? STATUS_JOIN_EVENT[1] : STATUS_JOIN_EVENT[0] }}</td>
+                    <td class="d-none d-sm-table-cell">{{ $listUserJoinEventValue->content}}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        <br/>
+    @endif
+
 @endsection
+@push('extend-js')
+    <script>
+        $(document).ready(function () {
+            $('.dataTables_length').addClass('bs-select');
+        });
+    </script>
+@endpush
 

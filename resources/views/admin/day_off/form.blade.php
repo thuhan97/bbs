a0177f25a1c2e9920427f13e0cf08832d53ce1b1
Tung Thanh <tung.jvb@gmail.com>
2019-06-04 11:33:43 +0700

    add

--- resources/views/admin/day_off/form.blade.php
+++ resources/views/admin/day_off/form.blade.php
@@ -3,7 +3,7 @@
         <div class="col-md-12">
             <div class="form-group margin-b-5 margin-t-5{{ $errors->has('user_id') ? ' has-error' : '' }}">
                 <label for="user_id">Chọn nhân viên *</label>
-                {{ Form::select('user_id', ['' => 'Chọn nhân viên'] +  $request_users, old('user_id',$record->user_id), $record->user_id ? ['disabled'=>'disabled','class'=>'select2 form-control', $errors->has('user_id') ? ' has-error' : '' ] : ['class'=>'select2 form-control', $errors->has('user_id') ? ' has-error' : '' ]) }}
+                {{ Form::select('user_id', ['' => 'Chọn nhân viên'] +  $request_users, old('user_id',$record->user_id), $record->user_id ? ['disabled'=>'disabled','class'=>'select2 form-control'] : ['class'=>'select2 form-control']) }}
                 @if ($errors->has('user_id'))
                     <span class="help-block">
                     <strong>{{ $errors->first('user_id') }}</strong>

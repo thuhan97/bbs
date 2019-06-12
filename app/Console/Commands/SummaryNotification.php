a919a6df6c838a064159d1a1a4f7b76ee8bd0466
Trình <trinhnv.jvb@gmail.com>
2019-06-12 10:37:04 +0700

    firebase notification

--- app/Console/Commands/SummaryNotification.php
+++ app/Console/Commands/SummaryNotification.php
@@ -39,17 +39,19 @@ class SummaryNotification extends Command
      */
     public function handle()
     {
-        $users = User::select('id', 'name')
-            ->where('status', ACTIVE_STATUS)
-            ->has('active_firebase_tokens')
-            ->with('active_firebase_tokens:user_id,token')
-            ->has('unread_notifications', '>', 0)
-            ->withCount('unread_notifications')
-            ->get();
-        foreach ($users as $user) {
-            $deviceTokens = $user->active_firebase_tokens->pluck('token')->toArray();
-            $content = "Bạn có $user->unread_notifications_count thông báo chưa đọc.";
-            NotificationHelper::sendPushNotification($deviceTokens, '[' . env('APP_NAME') . ']', $content);
+        if (env('ENABLE_PUSH_NOTIFY')) {
+            $users = User::select('id', 'name')
+                ->where('status', ACTIVE_STATUS)
+                ->has('active_firebase_tokens')
+                ->with('active_firebase_tokens:user_id,token')
+                ->has('unread_notifications', '>', 0)
+                ->withCount('unread_notifications')
+                ->get();
+            foreach ($users as $user) {
+                $deviceTokens = $user->active_firebase_tokens->pluck('token')->toArray();
+                $content = "Bạn có $user->unread_notifications_count thông báo chưa đọc.";
+                NotificationHelper::sendPushNotification($deviceTokens, '[' . env('APP_NAME') . ']', $content);
+            }
         }
     }
 }
